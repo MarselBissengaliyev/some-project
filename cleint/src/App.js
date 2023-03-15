06eda14d951ea869831ae965733046a0b11dd800
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-03-15 17:58:43 +0500

    change client

---
+++ cleint/src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [ip, setIp] = React.useState();
+  const params = window.location.search
+    .replace("?", "")
+    .split("&")
+    .reduce(function (p, e) {
+      var a = e.split("=");
+      p[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+      return p;
+    }, {});
+
+  const click_id = params.clickid;
+  const pixel = params.fbp;
+  const fb_click = params.fbclid;
+
+  const user_agent = window.navigator.userAgent;
+  const domain = window.location.hostname;
+  useEffect(() => {
+    fetch("https://ipapi.co/json/")
+      .then((d) => d.json())
+      .then((d) => {
+        setIp(d.ip);
+      });
+  }, []);
+  const handleClick = async () => {
+    await fetch("https://back.roiup.team/api/facebook-data", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        click_id,
+        ip,
+        user_agent,
+        pixel,
+        fb_click,
+        domain,
+        time_click: new Date(),
+      }),
+    })
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        window.location.href = `https://t.me/umnico_test2_bot?start=${click_id}`;
+      })
+      .catch(console.log());
+  };
+
+  if (!click_id || !fb_click || !pixel) {
+    return <h1>У вас нет нужных макросов, кнопка не сработает</h1>;
+  }
+  return (
+    <div className="App">
+      <button onClick={() => handleClick()}>Test</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ script.js
@@ -0,0 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const test = document.getElementById("test");
+
+  var params = window.location.search
+    .replace("?", "")
+    .split("&")
+    .reduce(function (p, e) {
+      var a = e.split("=");
+      p[decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+      return p;
+    }, {});
+
+    const message = document.getElementById("message");
+
+    console.log(params);
+
+  if (!params.clickid || !params.fbclid || !params.fbp) {
+    console.log()
+    message.innerHTML = `У вас нет нужных макросов, кнопка не сработает`;
+    return;
+  }
+
+  // Параметры
+  const click_id = params.click_id;
+  const pixel = params.fbp;
+  const fb_click = params.fbclid;
+
+  function setIp(value) {
+    ip = value;
+  }
+
+  async function getAPi() {
+    let ip;
+    await fetch("https://ipapi.co/json/")
+      .then((d) => d.json())
+      .then((d) => {
+        setIp(d.ip);
+      });
+
+    return ip;
+  }
+
+  console.log(getAPi());
+  const user_agent = document.getElementById("user_agent").value;
+  const domain = document.getElementById("domain").value;
+  const time_click = document.getElementById("time_click").value;
+
+  test.addEventListener("click", async (e) => {
+    e.preventDefault();
+    console.log(click_id, ip, user_agent, pixel, fb_click, domain, time_click);
+    await fetch("https://back.roiup.team/api/facebook-data", {
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      method: "POST",
+      body: JSON.stringify({
+        click_id,
+        ip,
+        user_agent,
+        pixel,
+        fb_click,
+        domain,
+        time_click,
+      }),
+    })
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        window.location.href = `https://t.me/umnico_test2_bot?start=${click_id}`;
+      })
+      .catch(console.log(err));
+  });
+});
