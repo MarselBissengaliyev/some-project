9c0971785a31f8c1ad7fd2853616f43875a90f37
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-03-18 23:30:52 +0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -86,7 +86,7 @@ function App() {
               to="/"
               className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
             >
-              <span className="fs-4">Навигация</span>
+              <span className="fs-4">Navigation</span>
             </Link>
             <hr />
             <ul className="nav nav-pills flex-column mb-auto">
