b8b03cbbf93d2c9e2ed39535a733364c67d54fd8
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-03-26 04:05:10 +0500

    set header

--- api/src/app.ts
+++ api/src/app.ts
@@ -41,6 +41,11 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://front.roiup.team");
+  next();
+})
+
 app.use("/api/facebook-data", facebookDataRoutes);
 
 app.use("/api/pixels", pixelRoutes);
