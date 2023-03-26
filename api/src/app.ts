e5f26fe778879beba0bc104336cb049a0fff19c2
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-03-27 01:11:43 +0500

    add whitelist

--- api/src/app.ts
+++ api/src/app.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(morgan("dev"));
 
-const whitelist = ["https://front.roiup.team", 'http://localhost:3000'];
+const whitelist = ["https://front.roiup.team", 'http://localhost:3000', 'https://klensowter.click'];
 
 app.use(
   cors({
