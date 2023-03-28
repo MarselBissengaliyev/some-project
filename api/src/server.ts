4ede74f85759232c80630c9720d5dd7a09f1bec6
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-03-28 23:57:50 +0500

    debug

--- api/src/server.ts
+++ api/src/server.ts
@@ -12,6 +12,7 @@ const server = http.createServer(app);
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
+    mongoose.set('debug', true);
     console.log("Mongoose connected");
     server.listen(port, () => {
       console.log(`Server running on port: ${port}`);
