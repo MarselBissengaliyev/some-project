824a14fe931182fd2c1c0dd91232daf89d09254e
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-03-29 21:06:40 +0500

    diasda

--- bot/src/controllers/facebookData/facebookData.ts
+++ bot/src/controllers/facebookData/facebookData.ts
@@ -21,22 +21,29 @@ export const postEvent = (
   { ip, user_agent, fb_click }: UserDataInterface,
   { pixel_id, token, domain }: EventRequestInterface
 ) => {
-  console.log(`fb.1.${fb_click.time_click}.${fb_click.value}`);
-  console.log(`https://${domain}`);
-  console.log(ip);
-  console.log(user_agent);
+  // const userData = new UserData()
+  //   .setClientIpAddress(ip)
+  //   .setClientUserAgent(user_agent)
+  //   .setFbc(`fb.1.${fb_click.time_click}.${fb_click.value}`);
+
+  // const serverEvent = new ServerEvent()
+  //   .setEventName("Lead")
+  //   .setEventTime(current_timestamp)
+  //   .setUserData(userData)
+  //   .setActionSource("website")
+  //   .setEventSourceUrl(`https://${domain}`);
 
   const userData = new UserData()
-    .setClientIpAddress(ip)
-    .setClientUserAgent(user_agent)
-    .setFbc(`fb.1.${fb_click.time_click}.${fb_click.value}`);
+  .setClientIpAddress("103.255.4.24")
+  .setClientUserAgent("Mozilla/5.0 (Linux; Android 11; V2043_21) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36")
+  .setFbc(`fb.1.1679914434.PAAabcPnGV7ReY6TMWJtT_Qpt13cpcm7wz-7LjrK51R31cTp8OCd7BlFxOdZ4_aem_Ae-EPbJM-u0GgiaoVUuNf6dxdDIZBy3yg9knryaxMCwZUHJQSANcw5rKwI_kIilzxnGsRb0Wir4l4Y5dXzNf8xQBvpK36WRgu9PY4_vVIubsMSYQb20gB77dyoUCDpkrk8tpr7ToFc6XyZYVPee7JjVc`);
 
-  const serverEvent = new ServerEvent()
-    .setEventName("Lead")
-    .setEventTime(current_timestamp)
-    .setUserData(userData)
-    .setActionSource("website")
-    .setEventSourceUrl(`https://${domain}`);
+const serverEvent = new ServerEvent()
+  .setEventName("Lead")
+  .setEventTime(current_timestamp)
+  .setUserData(userData)
+  .setActionSource("website")
+  .setEventSourceUrl(`https://softwerenews.click`);
 
   const eventsData = [serverEvent];
 
