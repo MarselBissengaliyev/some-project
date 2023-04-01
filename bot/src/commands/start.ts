8eeaaf8cd7e42638d6886cf907c94dbace92b7f1
Marsel Bisengaliev <marselbisengaliev1@gmail.com>
2023-04-01 10:58:05 +0500

    please work

--- bot/src/commands/start.ts
+++ bot/src/commands/start.ts
@@ -47,32 +47,34 @@ export const start = async (ctx: StartContext) => {
     return;
   }
 
-  if (startMessage.photo) {
-    const re = /(?:\.([^.]+))?$/;
-    const extension = startMessage.photo && re.exec(startMessage.photo);
-
-    if ((extension && extension[1]) === "gif") {
-      return await ctx.replyWithAnimation(
-        `${env.API_URL}${startMessage.photo}`,
-        {
-          caption: startMessage.message,
-          parse_mode: "Markdown",
-          disable_notification: false,
-        }
-      );
+  if (ctx.from) {
+    if (startMessage.photo) {
+      const re = /(?:\.([^.]+))?$/;
+      const extension = startMessage.photo && re.exec(startMessage.photo);
+  
+      if ((extension && extension[1]) === "gif") {
+        return await ctx.replyWithAnimation(
+          `${env.API_URL}${startMessage.photo}`,
+          {
+            caption: startMessage.message,
+            parse_mode: "Markdown",
+            disable_notification: false,
+          }
+        );
+      }
+  
+      await ctx.replyWithPhoto(`${env.API_URL}${startMessage.photo}`, {
+        caption: startMessage.message,
+        parse_mode: "Markdown",
+        disable_notification: false,
+      });
+    } else {
+      await ctx.reply(startMessage.message, {
+        disable_notification: false,
+        disable_web_page_preview: startMessage.disable_web_page_preview,
+        parse_mode: "Markdown",
+      });
     }
-
-    await ctx.replyWithPhoto(`${env.API_URL}${startMessage.photo}`, {
-      caption: startMessage.message,
-      parse_mode: "Markdown",
-      disable_notification: false,
-    });
-  } else {
-    await ctx.reply(startMessage.message, {
-      disable_notification: false,
-      disable_web_page_preview: startMessage.disable_web_page_preview,
-      parse_mode: "Markdown",
-    });
   }
 
   const clickId = ctx.startPayload;
